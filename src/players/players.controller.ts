af60b1cf5dec0f2ed79cfaaf03318fdfdd6c002d
luidimso <luidi_mso@outlook.com>
2021-05-25 12:01:26 -0300

    Player interface and create player DTO

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDTO } from './dtos/create-player.dto';
 
 @Controller('api/v1/players')
 export class PlayersController {
 
     @Post()
-    async createOrUpdatePlayer() {
+    async createOrUpdatePlayer(@Body() player:CreatePlayerDTO) {
         return JSON.stringify({
-            "name": "test"
+            "name": player.name
         })
     }
 }
